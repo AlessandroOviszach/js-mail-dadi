977c5d1e50f66a894a582e320546b22973127380
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-10-26 19:09:38 +0200

    gioco dei dadi

--- js/main.js
+++ js/main.js
@@ -1,32 +1,4 @@
-/**
-Esercizio di oggi: JS Mail e Dadi
-nome repo: js-mail-dadi
-
-Mail
-    - crea una lista contenente almeno 3 indirizzi email
-    - chiedi all’utente la sua email,
-    - controlla che sia nella lista di chi può accedere (lista creata sopra al punto 1),
-    - stampa un messaggio appropriato sull’esito del controllo.
-
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Ricordate che la documentazione è li per aiutarci: https://www.w3schools.com/js/js_random.asp
-
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che “cosa” ci serve?
-Consigli del giorno:
-    1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-    2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-    3. si ma noi cosa vogliamo fare?
-    4. torniamo a scrivere in italiano
-    5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
-Buon lavoro e a domani :faccia_leggermente_sorridente:
- */
-
-
-
+// controllo delle mail
 const mail_list = [
     'prova-mail-1@gmail.com',
     'prova-mail-2@gmail.com', 
@@ -48,4 +20,29 @@ access_btn.addEventListener('click', function() {
         }
     }
 });
+
+
+// gioco dei dadi
+const generation_btn = document.querySelector('.btn-generation');
+let num_utente
+let num_computer
+let vincitore
+
+generation_btn.addEventListener('click', function() {
+    num_utente = document.getElementById('num_utente').innerHTML = Math.floor(Math.random() * 6);
+
+    num_computer = document.getElementById('num_computer').innerHTML = Math.floor(Math.random() * 6);
+
+    // condizione utenza vincirice
+    if (num_utente > num_computer) {
+        vincitore = document.getElementById('vincitore').innerHTML = 'utente'
+
+      // condizione computer vincirice
+    } else if (num_utente < num_computer) {
+        vincitore = document.getElementById('vincitore').innerHTML = 'computer'
+    }
+});
+
+
+
 
